3817dc3dc89dbbd092c1df10ea1c045eb317e1e8
Aline Abler <alinea@riseup.net>
2017-02-24 16:21:09 +0100

    Weather icons support for conky

--- conky/meteo/generateplot.py
+++ conky/meteo/generateplot.py
@@ -7,6 +7,79 @@ import json
 import itertools
 import datetime as dt
 from matplotlib import ticker
+# Map between Meteoswiss icon IDs and ConkyWeather font letters
+weathericons = {
+    1: "a",
+    2: "b",
+    3: "c",
+    4: "d",
+    5: "f",
+    6: "g",
+    7: "o",
+    8: "o",
+    9: "g",
+    10: "o",
+    11: "o",
+    12: "k",
+    13: "k",
+    14: "h",
+    15: "p",
+    16: "p",
+    17: "i",
+    18: "p",
+    19: "q",
+    20: "j",
+    21: "q",
+    22: "q",
+    23: "l",
+    24: "m",
+    25: "n",
+    26: "b",
+    27: "9",
+    28: "0",
+    29: "g",
+    30: "o",
+    31: "o",
+    32: "g",
+    33: "g",
+    34: "o",
+    35: "e",
+    101: "A",
+    102: "B",
+    103: "C",
+    104: "D",
+    105: "f",
+    106: "G",
+    107: "O",
+    108: "O",
+    109: "G",
+    110: "O",
+    111: "O",
+    112: "K",
+    113: "K",
+    114: "h",
+    115: "p",
+    116: "p",
+    117: "i",
+    118: "p",
+    119: "q",
+    120: "j",
+    121: "q",
+    122: "q",
+    123: "l",
+    124: "m",
+    125: "n",
+    126: "B",
+    127: "9",
+    128: "0",
+    129: "G",
+    130: "O",
+    131: "O",
+    132: "G",
+    133: "G",
+    134: "O",
+    135: "e"
+}
 
 # create a time format string from an unix timestamp
 def format_time(time, pos=None):
@@ -21,6 +94,9 @@ f.close()
 temps = [ row[1]           for k in [0,1] for row in j[k].get("temperature") ]
 precs = [ row[1]           for k in [0,1] for row in j[k].get("rainfall")    ]
 times = [ int(row[0]/1000) for k in [0,1] for row in j[k].get("temperature") ]
+
+# Extract icon information
+icons = [ {"time":row.get("timestamp")/1000, "icon":weathericons[row.get("weather_symbol_id")]} for k in [0,1] for row in j[k].get("symbols") ]
 
 # current time as reported by json, in seconds
 now = j[0].get("current_time") / 1000
@@ -48,6 +124,7 @@ for tl in ax1.get_xticklabels():
     tl.set_color('w') # set all x tick labels to white
 ax1.xaxis.set_major_formatter(ticker.FuncFormatter(format_time)) # make the x axis convert unix timestamps to time strings
 ax1.xaxis.set_ticks(xticks) # set tick interval according to our xticks list
+ax1.spines['top'].set_visible(False) # remove top border
 
 # Plot precipitation
 ax1.bar([x-1750 for x in times], precs, 3500, color='#aaaaff')
@@ -58,8 +135,14 @@ ax1.set_facecolor('k')
 for tl in ax1.get_yticklabels():
     tl.set_color('w')
 
+# Plot weather icons
+for icon in icons:
+    if starttime < icon["time"] and icon["time"] < endtime:
+        ax1.text(icon["time"] - 1800, 7.5, icon["icon"], color='w', fontname="ConkyWeather", fontstyle='oblique', fontsize=20)
+
 # Add new y axis and plot temperature
 ax2 = ax1.twinx()
+ax2.spines['top'].set_visible(False) # remove top border
 ax2.plot(times, temps, 'w',  linewidth=3)
 ax2.set_ylim([mintemp - 2, maxtemp + 2])  # y axis range
 ax2.vlines(now, mintemp - 2, maxtemp + 1, colors='w', linestyles='dotted') # vertical line where the current time is
